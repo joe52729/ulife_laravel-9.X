972881f29028a6af8e7ceaaf440bcfef0d8dfb88
Arvin <joe52729@gmail.com>
2022-04-08 00:43:48 +0800

    [DB]商品migrate建立

---
+++ database/migrations/2022_04_07_080758_product.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            //$table->id()->unique();
+            $table->string('goods_id', 20)->comment('貨物ID');
+            $table->string('goods_num', 20)->comment('貨物編號');
+            $table->string('goods_name', 20)->comment('商品名稱');
+            $table->string('goods_descript', 100)->comment('商品描述');
+            $table->integer('goods_amount', 30)->comment('商品數量');
+            $table->string('goods_price', 30)->comment('貨物單價');
+            $table->dateTime('created_at')->commnet('上架時間');
+            $table->date('updated_at')->commnet('最後更新時間');
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
